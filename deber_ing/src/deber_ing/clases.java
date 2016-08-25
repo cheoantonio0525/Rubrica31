1c47f772a2a85211aa38bfc7dd83cdc278bc9605
admin <admin@MSI>
2016-08-25 15:53:30 -0500

    crearcion de clases, parametros y los metodos get y setde las mismas clases

---
+++ deber_ing/src/deber_ing/clases.java
@@ -0,0 +1,60 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package deber_ing;
+
+/**
+ *
+ * @author admin
+ */
+public class clases {
+     private String casa;
+     private String carro;
+     private String edificio;
+
+    /**
+     * @return the casa
+     */
+    public String getCasa() {
+        return casa;
+    }
+
+    /**
+     * @param casa the casa to set
+     */
+    public void setCasa(String casa) {
+        this.casa = casa;
+    }
+
+    /**
+     * @return the carro
+     */
+    public String getCarro() {
+        return carro;
+    }
+
+    /**
+     * @param carro the carro to set
+     */
+    public void setCarro(String carro) {
+        this.carro = carro;
+    }
+
+    /**
+     * @return the edificio
+     */
+    public String getEdificio() {
+        return edificio;
+    }
+
+    /**
+     * @param edificio the edificio to set
+     */
+    public void setEdificio(String edificio) {
+        this.edificio = edificio;
+    }
+     
+}
+
