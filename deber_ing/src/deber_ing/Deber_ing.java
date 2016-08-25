ff12674c29975a337ba87d295b9442ab25ba7a8e
admin <admin@MSI>
2016-08-25 15:35:33 -0500

    creacion del proyecto

---
+++ deber_ing/src/deber_ing/Deber_ing.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package deber_ing;
+
+/**
+ *
+ * @author admin
+ */
+public class Deber_ing {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        System.out.println("hola mundo");
+    }
+    
+}
